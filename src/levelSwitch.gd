21fac1f0f8e02eb782989df55cd487b503306df5
ApianbelleDev <81694696+ApianbelleDev@users.noreply.github.com>
2022-07-17 21:59:09 -0400

    added mega man style movement

--- src/levelSwitch.gd
+++ src/levelSwitch.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 #TODO: create a variable in a global script for the game to tell which level you are currently on
+export (String) var levelName
 
 
 #calles levelChange on kinematicbody2D collision with the level end component
@@ -9,4 +10,4 @@ func _on_player_entered(_body):
 
 #gets a scene to change based on a variable
 func levelChange():
-	get_tree().change_scene("res://src/level_1.tscn")
+	get_tree().change_scene(levelName)
